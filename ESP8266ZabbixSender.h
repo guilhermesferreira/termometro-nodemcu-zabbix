16a2b3e64aa17dc09844382654df943e99363317
Guilherme Siqueira Ferreira <45656415+guilhermesferreira@users.noreply.github.com>
2023-04-18 15:39:59 -0300

    Alterado o tipo de dado da função ESP8266ZabbixSender:AddItem
    ESP8266ZabbixSender::AddItem(String key, String value)

--- ESP8266ZabbixSender.h
+++ ESP8266ZabbixSender.h
@@ -1,5 +1,5 @@
-#ifndef _ESP8266ZABBIXSENDER_H_
-#define _ESP8266ZABBIXSENDER_H_
+#ifndef _ESP8266ZabbixSender_H_
+#define _ESP8266ZabbixSender_H_
 
 #if ARDUINO >= 100
 #include "Arduino.h"
@@ -8,9 +8,9 @@
 #endif
 
 #include <ESP8266WiFi.h>
-#define ZABBIXMAXLEN 256 // max 256byte packet
-#define ZABBIXMAXITEM 50 // max 50 items
-#define ZABBIXTIMEOUT 1000 // 1000ms
+#define ZABBIXMAXLEN 600 // max 256byte packet
+#define ZABBIXMAXITEM 1000 // max 50 items
+#define ZABBIXTIMEOUT 2000 // 1000ms
 
 class ESP8266ZabbixSender {
 public:
@@ -18,13 +18,13 @@ public:
 	void Init(IPAddress ZabbixServerAddr, uint16_t ZabbixServerPort, String ZabbixItemHostName);
 	int Send(void);
 	void ClearItem(void);
-	void AddItem(String key, float value);
+	void AddItem(String key, String value);
 
 private:
 	int createZabbixPacket(void);
 	struct zabbixCell {
 		String key;
-		float val;
+		String val;
 	};
 	WiFiClient zClient;
 	IPAddress zAddr;
